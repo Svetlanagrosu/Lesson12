95fe4f54f5053eb6478bdb8c7f1eb705bfdf5995
Svetlanagrosu <grosu.svetlana.miha@gmail.com>
2024-03-02 22:21:53 +0100

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+let num1 = 6
+let num2 = 8
+
+num1++
+console.log(num1++)
+
+num1+
+console.log(num1)
+
+num1-
+console.log(num1)
+
+num1--
+console.log(num1)
+
+num1-num2
+console.log(num1-num2)
+
+num1+num2
+console.log(num1+num2)
+
+num1 / num2
+console.log(num1/num2)
+
+num1 * num2
+console.log(num1*num2)
+
+num1 > num2
+console.log(num1 > num2)
+
+num1 <= num2
+console.log(num1<=num2)
+
+num1 >= num2
+console.log(num1>=num2)
+
+num1 == num2
+console.log(num1==num2)
+
 
